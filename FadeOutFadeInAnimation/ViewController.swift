b05721133f9f4423c9323e60feb3b2d423a8d37e
minalkewat <minalkewat@gmail.com>
2019-06-11 16:43:55 +0530

    Initial Commit

---
+++ FadeOutFadeInAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FadeOutFadeInAnimation
+//
+//  Created by Meenal Kewat on 6/11/19.
+//  Copyright © 2019 Meenal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
